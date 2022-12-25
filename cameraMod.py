ffac8849b843802bec93fc4dfba3f03e98fe9f03
Dhairya RPi <dtriv97@gmail.com>
2022-12-25 22:05:01 +1300

    Improved workflow for basic script

--- cameraMod.py
+++ cameraMod.py
@@ -1,11 +1,38 @@
+import os
 from picamera import PiCamera
-import time
+from time import sleep
 
+"""
+------------ Setup Vars ------------
+"""
+# Time taken for Pi Camera to intialise
+camInitTime = 2
+
+outputFolder = "Output/"
+
+# Image file output name
+imgOutput = outputFolder + "img.jpg"
+
+# Main object for camera
 camera = PiCamera()
-camera.resolution = (1920, 1080)
-camera.vflip = True
-camera.hflip = True
-camera.contrast = 10
 
-time.sleep(2)
-camera.capture("img.jpg")
+"""
+------------- Main Code -------------
+"""
+def init():
+    if os.path.isdir(outputFolder):
+        pass
+    else:
+        os.mkdir(outputFolder)
+
+def main():
+    camera.resolution = (1920, 1080)
+    camera.vflip = True
+    camera.hflip = True
+    camera.contrast = 10
+    sleep(camInitTime)
+    camera.capture(imgOutput)
+
+if __name__ == '__main__':
+    init()
+    main()
