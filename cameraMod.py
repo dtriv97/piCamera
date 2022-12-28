eb0b74b1b975f6b1bb84ba0e0ec365dc44bcc68e
Dhairya RPi <dtriv97@gmail.com>
2022-12-28 23:02:21 +1300

    Restructure and add long expo mode

--- cameraMod.py
+++ cameraMod.py
@@ -1,5 +1,6 @@
 import os
 from picamera import PiCamera
+from fractions import Fraction
 from time import sleep
 
 """
@@ -13,26 +14,45 @@ outputFolder = "Output/"
 # Image file output name
 imgOutput = outputFolder + "img.jpg"
 
-# Main object for camera
-camera = PiCamera()
-
 """
 ------------- Main Code -------------
 """
-def init():
+def init(initType: str = None) -> PiCamera:
     if os.path.isdir(outputFolder):
         pass
     else:
         os.mkdir(outputFolder)
 
-def main():
+    camera = PiCamera()
+
+    # Common camera settings for all mode types
     camera.resolution = (1920, 1080)
-    camera.vflip = True
-    camera.hflip = True
     camera.contrast = 10
-    sleep(camInitTime)
+    camera.hflip = True
+    camera.vflip = True
+    sleep(5)
+
+    # Special parameters for long exposure image
+    if initType == 'longExpo':
+        camera.framerate = Fraction(1, 30)
+        camera.sensor_mode = 3
+        camera.shutter_speed = 500000
+        camera.iso = 400
+        camera.exposure_mode = 'off'
+        sleep(30)
+
+    return camera
+
+def captureImage(camera):
     camera.capture(imgOutput)
+    print("Image captured")
+    sleep(6)
+    camera.close()
+    print("Camera closed")
+
+def main():
+    camera = init('longExpo')
+    captureImage(camera)
 
 if __name__ == '__main__':
-    init()
     main()
